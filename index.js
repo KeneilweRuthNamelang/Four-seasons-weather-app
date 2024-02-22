b908057fab505439226b519bf82f885afdcb1e6a
Keneilwe Namelang <145266192+KeneilweRuthNamelang@users.noreply.github.com>
2024-02-22 15:17:36 +0200

    Fixed the search function using api

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function conductSearch (event){
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
+function updateWeather(response) {
+  let currentTemperatureElement = document.querySelector("#current-temperature");
+  let currentTemperature = response.data.temperature.current;
   let cityElement = document.querySelector("#searched-city");
-  cityElement.innerHTML = searchInput.value;
+
+  cityElement.innerHTML = response.data.city;
+  currentTemperatureElement.innerHTML = Math.round(currentTemperature);
 }
 
+function searchCity(city) {
+  let apiKey = "117da7948a04330f7fb96d315918tcdo";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 
-let searchFormElement = document.querySelector ("#search-form");
+function conductSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  
+  
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", conductSearch);
+
+searchCity("Welkom")
