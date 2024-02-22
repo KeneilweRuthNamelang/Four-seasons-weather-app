921625ecbc5c69ad20c4a856959d982fc3a514c5
Keneilwe Namelang <145266192+KeneilweRuthNamelang@users.noreply.github.com>
2024-02-22 14:03:04 +0200

    edited css,html and js files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-df
+function conductSearch (event){
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#searched-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector ("#search-form");
+searchFormElement.addEventListener("submit", conductSearch);
