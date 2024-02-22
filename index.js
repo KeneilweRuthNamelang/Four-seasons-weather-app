b75dedd0d60357307287429e1b08bd3526899994
Keneilwe Namelang <145266192+KeneilweRuthNamelang@users.noreply.github.com>
2024-02-22 23:57:13 +0200

    added descriptions to weather

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 function updateWeather(response) {
-  let currentTemperatureElement = document.querySelector("#current-temperature");
+  let currentTemperatureElement = document.querySelector(
+    "#current-temperature"
+  );
   let currentTemperature = response.data.temperature.current;
   let cityElement = document.querySelector("#searched-city");
+  let descriptionElement = document.querySelector("#sky-description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
 
   cityElement.innerHTML = response.data.city;
   currentTemperatureElement.innerHTML = Math.round(currentTemperature);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
 }
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let timeElement = document.querySelector("#current-time");
+let date = new Date();
+timeElement.innerHTML = formatDate(currentTime);
 
 function searchCity(city) {
   let apiKey = "117da7948a04330f7fb96d315918tcdo";
@@ -16,12 +55,11 @@ function searchCity(city) {
 function conductSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  
-  
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", conductSearch);
 
-searchCity("Welkom")
+searchCity("Welkom");
